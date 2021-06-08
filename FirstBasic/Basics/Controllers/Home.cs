2222fb9e5e6be397f155529ea1de3a06930d2b3d
Dominik Wikliński <domel2222@gmail.com>
2021-06-08 08:03:39 +0200

    added injection to index.cshtml IAuthorizationService

--- FirstBasic/Basics/Controllers/Home.cs
+++ FirstBasic/Basics/Controllers/Home.cs
@@ -12,6 +12,13 @@ namespace Basics.Controllers
 {
     public class Home : Controller
     {
+        private readonly IAuthorizationService _authorizationService;
+
+        public Home(IAuthorizationService authorizationService)
+        {
+            this._authorizationService = authorizationService;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -36,25 +43,25 @@ namespace Basics.Controllers
         }
         public IActionResult Authenticate()
         {
-            var grandmaClaims = new List<Claim>()
+            var supaClaims = new List<Claim>()
             {
                 new Claim(ClaimTypes.Name, "Bob"),
                 new Claim(ClaimTypes.Email, "Bob@fmail.com"),
                 new Claim(ClaimTypes.DateOfBirth, "11/11/2001"),
                 new Claim(ClaimTypes.Role, "Admin"),
-                new Claim("Grandma.Says", "Good Boys"),
+                new Claim("Jhons.Says", "Good Boys"),
             };
             var licenseClaims = new List<Claim>()
             {
-                new Claim(ClaimTypes.Name, "Bob K Foo"),
+                new Claim(ClaimTypes.Name, "Archi K Boomm"),
                 new Claim("DrivingLicencse", "A+")
             };
 
-            var grandmaIdentity = new ClaimsIdentity(grandmaClaims, "Grandma Identity");
+            var personalIdentity = new ClaimsIdentity(supaClaims, "Jhons Identity");
             var licenseIdentity = new ClaimsIdentity(licenseClaims, "Government");
 
 
-            var userPrincipal = new ClaimsPrincipal(new[] { grandmaIdentity, licenseIdentity });
+            var userPrincipal = new ClaimsPrincipal(new[] { personalIdentity, licenseIdentity });
 
 
             //------------------------------------------------------------------------------------------
@@ -62,6 +69,24 @@ namespace Basics.Controllers
 
             HttpContext.SignInAsync(userPrincipal);
             return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> DoTheDishes()
+        {
+
+            // we are doing stuff here 
+            var builder = new AuthorizationPolicyBuilder("Schema");
+            var customPolicy = builder.RequireClaim("Hello").Build();
+
+
+
+            var authResult = await _authorizationService.AuthorizeAsync(User, customPolicy);
+
+            if (authResult.Succeeded)
+            {
+
+            }
+            return View("Index");
         }
     }
 }
