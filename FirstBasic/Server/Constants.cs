84feae7f4f0a50e90d8f96c655c98b697c90f1ed
Dominik Wikliński <domel2222@gmail.com>
2021-06-09 11:19:54 +0200

    Added Constants class with parts for JTW

---
+++ FirstBasic/Server/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Constants
+    {
+        public const string Audiance = "https://localhost:44363/";
+        public const string Issuer = Audiance;
+    }
+}
