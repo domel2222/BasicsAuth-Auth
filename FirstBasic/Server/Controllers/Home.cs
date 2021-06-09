0b6b7b129281085cf60c54fa4b10896637440912
Dominik Wikliński <domel2222@gmail.com>
2021-06-09 11:13:35 +0200

    addded jwt to authorize method

---
+++ FirstBasic/Server/Controllers/Home.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace Server.Controllers
+{
+    public class Home : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        
+        public IActionResult Authenticate()
+        {
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "some_id"),
+                new Claim("wookie", "cookie")
+
+            };
+
+            //JsonConvert.DeserializeObject<TypeFilterAttribute>
+
+            var token = new JwtSecurityToken();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
--- FirstBasic/Server/Startup.cs
+++ FirstBasic/Server/Startup.cs
@@ -12,28 +12,47 @@ namespace Server
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+            services.AddAuthentication("OAuth")
+                .AddJwtBearer("OAuth", config =>
+                {
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+                });
+            services.AddControllersWithViews();
+
+
+            services.AddHealthChecks(); /// what is it???
+
+
+
+        }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+
+
             app.UseRouting();
+
+            //who you are?
+
+            app.UseAuthentication();
+
+            //are you allowed?
+            app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
+                //endpoints.MapRazorPages();
+                //endpoints.MapHealthChecks();
             });
         }
     }
