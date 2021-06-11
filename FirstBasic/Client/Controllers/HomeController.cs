f5132993d9bdcff01d65018a21f1502bbc079d7e
Dominik Wikliński <domel2222@gmail.com>
2021-06-11 07:16:02 +0200

    added api to solution

--- FirstBasic/Client/Controllers/HomeController.cs
+++ FirstBasic/Client/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,12 @@ namespace Client.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient __client;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            __client = httpClientFactory.CreateClient(); 
+        }
         public IActionResult Index()
         {
             return View();
@@ -21,6 +28,10 @@ namespace Client.Controllers
         public async Task<IActionResult> Secret()
         {
             var token = await HttpContext.GetTokenAsync("access_token");
+
+            __client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+
+            var serverResponse = await __client.GetAsync("https://localhost:44363/");
 
             return View();
         }
