df80d70a5c3243b1dd24756c5ae06e82f5a5761f
Dominik Wikliński <domel2222@gmail.com>
2021-06-14 08:41:39 +0200

    added HomeController moreover install IdentityServer

---
+++ FirstBasic/ApiTwo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiTwo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(IHttpClientFactory httpClientFactory)
+        {
+            this._httpClientFactory = httpClientFactory;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            //retrive access token
+
+            //retrive secret data
+
+
+            return Ok(new
+            {
+
+            });
+        }
+    }
+}
